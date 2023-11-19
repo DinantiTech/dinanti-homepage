e78765e2ae36affb5d26faf49215cce86fa3c60a
Saepudi <saepudinjeh@gmail.com>
2023-11-19 19:56:44 +0700

    update robbots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,9 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*'
+      userAgent: '*',
+      disallow: '/private/',
+      allow: '/',
     },
     sitemap: 'https://www.dinanti.id/sitemap.xml',
   }
