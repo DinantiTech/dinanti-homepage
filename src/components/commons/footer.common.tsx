dfe745f5b743740d99898e40cd7677403d982d16
Saepudi <saepudinjeh@gmail.com>
2023-12-05 21:49:33 +0700

    add login modal

--- src/components/commons/footer.common.tsx
+++ src/components/commons/footer.common.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full bg-[#2D2D2D] py-4 flex justify-between items-center text-white px-10 rounded-t-3xl text-xs">
+        <footer className="w-full bg-[#2D2D2D] mx-auto py-4 flex justify-between items-center text-white px-10 rounded-t-3xl text-xs z-40 flex-row relative flex-nowrap h-[var(--navbar-height)] max-w-[1024px]">
             <p>Built with &#10084; by Dinanti</p>
             <p>business@dinanti.id</p>
         </footer>
--- src/components/commons/navbar.common.tsx
+++ src/components/commons/navbar.common.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import LogoDinanti from "@/assets/logo.svg";
 import Image from "next/image";
+import ModalAuth from "../micro/modalAuth.micro";
 
 export default function NavbarCustom() {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
 
     const menuItems = [
         "Home",
@@ -14,73 +16,77 @@ export default function NavbarCustom() {
     ];
 
     return (
-        <Navbar
-            isBordered
-            isMenuOpen={isMenuOpen}
-            onMenuOpenChange={setIsMenuOpen}
-        >
-            <NavbarContent className="sm:hidden" justify="start">
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-            </NavbarContent>
+        <>
+            <ModalAuth isOpen={isOpenModal} handeModal={setIsOpenModal} />
+            <Navbar
+                isBordered
+                isMenuOpen={isMenuOpen}
+                onMenuOpenChange={setIsMenuOpen}
+            >
 
-            <NavbarContent className="sm:hidden pr-3" justify="center">
-                <NavbarBrand>
-                    <Link href="/">
-                        <Image src={LogoDinanti} alt="Logo Dinanti" width={500} height={500} className="w-20" />
-                    </Link>
-                </NavbarBrand>
-            </NavbarContent>
+                <NavbarContent className="sm:hidden" justify="start">
+                    <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+                </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarBrand>
-                    <Link href="/">
-                        <Image src={LogoDinanti} alt="Logo Dinanti" width={500} height={500} className="w-20" />
-                    </Link>
-                </NavbarBrand>
-            </NavbarContent>
+                <NavbarContent className="sm:hidden pr-3" justify="center">
+                    <NavbarBrand>
+                        <Link href="/">
+                            <Image src={LogoDinanti} alt="Logo Dinanti" width={500} height={500} className="w-20" />
+                        </Link>
+                    </NavbarBrand>
+                </NavbarContent>
 
-            <NavbarContent justify="end">
-                <NavbarContent justify="end" className="hidden sm:flex gap-10 mr-12">
+                <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                    <NavbarBrand>
+                        <Link href="/">
+                            <Image src={LogoDinanti} alt="Logo Dinanti" width={500} height={500} className="w-20" />
+                        </Link>
+                    </NavbarBrand>
+                </NavbarContent>
+
+                <NavbarContent justify="end">
+                    <NavbarContent justify="end" className="hidden sm:flex gap-10 mr-12">
+                        <NavbarItem>
+                            <Link href="/" className="text-[#353] hover:font-bold hover:text-black">Home</Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Link href="/harga" className="text-[#353] hover:font-bold hover:text-black" aria-current="page">Harga</Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Link href="#" className="text-[#353] hover:font-bold hover:text-black">Tema</Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Link href="#" className="text-[#353] hover:font-bold hover:text-black">Blog</Link>
+                        </NavbarItem>
+                    </NavbarContent>
+
                     <NavbarItem>
-                        <Link href="/" className="text-[#353] hover:font-bold hover:text-black">Home</Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link href="/harga" className="text-[#353] hover:font-bold hover:text-black" aria-current="page">Harga</Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link href="#" className="text-[#353] hover:font-bold hover:text-black">Tema</Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link href="#" className="text-[#353] hover:font-bold hover:text-black">Blog</Link>
+                        <Button onPress={() => setIsOpenModal(true)} size="md" href="#" variant="flat" radius="full" className="bg-[#1D1D1D] text-white font-bold hidden sm:flex">
+                            Login / Daftar
+                        </Button>
+                        <Button onPress={() => setIsOpenModal(true)} size="sm" href="#" variant="flat" radius="full" className="bg-[#1D1D1D] text-white font-bold sm:hidden">
+                            Login / Daftar
+                        </Button>
                     </NavbarItem>
                 </NavbarContent>
 
-                <NavbarItem>
-                    <Button as={Link} size="md" href="#" variant="flat" radius="full" className="bg-[#1D1D1D] text-white font-bold hidden sm:flex">
-                        Login / Daftar
-                    </Button>
-                    <Button as={Link} size="sm" href="#" variant="flat" radius="full" className="bg-[#1D1D1D] text-white font-bold sm:hidden">
-                        Login / Daftar
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-                            className="w-full"
-                            color={
-                                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-                            }
-                            href="#"
-                            size="lg"
-                        >
-                            {item}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-            </NavbarMenu>
-        </Navbar>
+                <NavbarMenu>
+                    {menuItems.map((item, index) => (
+                        <NavbarMenuItem key={`${item}-${index}`}>
+                            <Link
+                                className="w-full"
+                                color={
+                                    index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                                }
+                                href="#"
+                                size="lg"
+                            >
+                                {item}
+                            </Link>
+                        </NavbarMenuItem>
+                    ))}
+                </NavbarMenu>
+            </Navbar>
+        </>
     );
 }
