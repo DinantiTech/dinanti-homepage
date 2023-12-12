3ba279fd2ddfc296d0ae97d9462126f1fcef710c
Saepudi <saepudinjeh@gmail.com>
2023-12-12 19:37:12 +0700

    add main carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import FeatureSection from '@/components/sections/features.section';
 import HotLinkSection from '@/components/sections/hotLink.section';
 import HowToUseSection from '@/components/sections/howToUse.section';
+import MainCarousel from '@/components/sections/mainCarousel.section';
 import { NextUIProvider } from '@nextui-org/react';
 import { Montserrat } from 'next/font/google';
 
@@ -16,6 +17,8 @@ export default function Home() {
   return (
     <NextUIProvider>
       <div style={{ backgroundImage: `url("/line.svg")` }} className={`${montserrat.className} w-full bg-cover bg-center bg-no-repeat min-h-screen pb-20`}>
+        <MainCarousel />
+
         <div className='h-full w-full flex items-center justify-center'>
           <FeatureSection />
         </div>
---
+++ src/components/sections/mainCarousel.section.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, EffectCoverflow, Pagination } from 'swiper/modules';
+import Image from 'next/image';
+import { Button } from '@nextui-org/react';
+import { useRef } from 'react';
+
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css';
+
+import Container from "../commons/container.common";
+import Image1 from "@/assets/previews/preview_1.png";
+import Image2 from "@/assets/previews/preview_2.png";
+
+export default function MainCarousel() {
+    const swiperRef: any = useRef();
+
+    return (
+        <Container>
+            <div className="flex flex-col items-center justify-center w-full mt-10 mb-5">
+                <h1 className='font-bold text-5xl text-center w-2/3 my-5'>Buat Undangan Nikah Digital Anti Ribet</h1>
+                <p className='mb-10 font-medium text-md'>Lihat Tema Terbaru Kami</p>
+
+                <Swiper
+                    effect='coverflow'
+                    centeredSlides
+                    loop
+                    slidesPerView={3}
+                    coverflowEffect={
+                        { rotate: 0, stretch: -20, depth: 80, modifier: 2.5 }
+                    }
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className='w-[48rem] relative'
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    onSwiper={(swiper) => {
+                        swiperRef.current = swiper;
+                    }}
+                    style={{
+                        "--swiper-pagination-color": "#8ECA8D",
+                        "--swiper-pagination-bullet-inactive-color": "#999999",
+                        "--swiper-pagination-bullet-inactive-opacity": "1",
+                        "--swiper-pagination-bullet-size": "12px",
+                        "--swiper-pagination-bullet-horizontal-gap": "6px",
+                      }}
+                >
+                    <SwiperSlide className='rounded-2xl overflow-hidden'>
+                        <div className='w-full h-full relative flex flex-col items-center justify-center'>
+                            <Image src={Image2} alt='preview 1' className='w-full' />
+
+                            <div className='absolute bottom-3 w-full px-4'>
+                                <Button radius='full' fullWidth className='font-semibold text-white bg-[#1D1D1D]'>Lihat Tema</Button>
+                            </div>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide className='rounded-2xl overflow-hidden'>
+                        <div className='w-full h-full relative flex flex-col items-center justify-center'>
+                            <Image src={Image1} alt='preview 2' className='w-full' />
+
+                            <div className='absolute bottom-3 w-full px-4'>
+                                <Button radius='full' fullWidth className='font-semibold text-white bg-[#1D1D1D]'>Lihat Tema</Button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide className='rounded-2xl overflow-hidden'>
+                        <div className='w-full h-full relative flex flex-col items-center justify-center'>
+                            <Image src={Image1} alt='preview 1' className='w-full' />
+
+                            <div className='absolute bottom-3 w-full px-4'>
+                                <Button radius='full' fullWidth className='font-semibold text-white bg-[#1D1D1D]'>Lihat Tema</Button>
+                            </div>
+                        </div>
+
+                    </SwiperSlide>
+
+
+                    <div className='flex gap-x-28 items-center justify-center mt-12'>
+                        <div className='swiper-pagination' />
+                    </div>
+                </Swiper>
+            </div>
+        </Container>
+    )
+}
