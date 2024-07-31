3f02ebee59ab00318baa390c9d5fcc83dd904177
wkwkw <saepudinjeh@gmail.com>
2024-08-01 00:38:45 +0700

    update generate robbots txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,18 @@
-import { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from 'next'
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://dinanti.id/sitemap.xml',
   }
 }
