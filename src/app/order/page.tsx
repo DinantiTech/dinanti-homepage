ae5c79b171fd8b046c503a162fb5cc17f30524ef
wkwkw <saepudinjeh@gmail.com>
2024-08-01 00:23:04 +0700

    update meta order

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,3 +1,6 @@
+"use server";
+
+import { Metadata } from "next";
 import dynamic from "next/dynamic"
 import { Suspense } from "react";
 
@@ -7,4 +10,16 @@ export default function MaintenancePage() {
     return (
         <Suspense> <Maintenance /> </Suspense>
     )
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Pesan Undangan - Dinanti",
+        description: "Pesan undangan digital yang elegan dan personal untuk berbagai acara Anda hanya di Dinanti. Mudah, cepat, dan terjangkau.",
+        keywords: "pesan undangan digital, undangan online, undangan pernikahan, undangan ulang tahun, undangan acara, Dinanti",
+        classification: "digital invitation",
+        alternates: {
+            canonical: "https://dinanti.id/order",
+        },
+    }
 }
