78c6cb96688c316f352f6076508ab35cace4e752
wkwkw <saepudinjeh@gmail.com>
2024-12-23 14:30:44 +0700

    update create invitation button on navbar

--- src/components/buttons/oauth.btn.tsx
+++ src/components/buttons/oauth.btn.tsx
@@ -3,41 +3,41 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useGoogleLogin } from "@react-oauth/google";
 import Link from "next/link";
-import { cookies } from 'next/headers';
 import { useRouter } from "next/navigation";
 import StorageUtil from "@/libs/helpers/storage.helper";
-import { useEffect } from "react";
 
 export default function ButtonCreate(props: PropsButtonCreateType) {
 
     const route = useRouter()
 
-    const googleLogin = useGoogleLogin({
-        flow: 'auth-code',
-        onSuccess: async codeResponse => {
-            const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL_DINANTI}/auth/login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ code: codeResponse?.code })
-            })
+    // const googleLogin = useGoogleLogin({
+    //     flow: 'auth-code',
+    //     onSuccess: async codeResponse => {
+    //         const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL_DINANTI}/auth/login`, {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({ code: codeResponse?.code })
+    //         })
 
-            if (data.status !== 200) {
-                return route.push('/')
-            }
+    //         if (data.status !== 200) {
+    //             return route.push('/')
+    //         }
 
-            const result = await data?.json();
+    //         const result = await data?.json();
 
-            StorageUtil.setItem({ type: 'cookie', key: 'crd', value: result?.data });
+    //         StorageUtil.setItem({ type: 'cookie', key: 'crd', value: result?.data });
 
-            console.log("token", result);
-        }
-    });
+    //         console.log("token", result);
+    //     }
+    // });
+
+    const handleCreateInvitation = () => route.push('/maintenance')
 
     return (
         <>{!props.is_maintenance ? (
-            <button onClick={googleLogin} name="button login" className="btn btn-xs xxs:btn-sm lg:btn-md group-hover:text-lime-900 rounded-lg bg-MIDNIGHT text-white hover:bg-NEUTRAL text-[0.6rem] sm:text-xs md:text-sm">
+            <button onClick={handleCreateInvitation} name="button login" className="btn btn-xs xxs:btn-sm lg:btn-md group-hover:text-lime-900 rounded-lg bg-MIDNIGHT text-white hover:bg-NEUTRAL text-[0.6rem] sm:text-xs md:text-sm">
                 {props?.icon_txt ? (<Icon icon={props?.icon_txt} />) : null}
                 <span className="hidden xxss:block">{props?.title}</span>
             </button>
