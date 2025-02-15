775a90cd85a72078fdf6a8ef0d394f1f043a1850
agung saepudin <saepudinjeh@gmail.com>
2025-02-15 19:35:16 +0700

    fix not found client_id

--- src/libs/providers/index.provider.tsx
+++ src/libs/providers/index.provider.tsx
@@ -7,7 +7,7 @@ import { ReactNode, useState } from "react";
 export default function IndexProvider({ children }: { children: ReactNode }) {
     const [client] = useState(new QueryClient());
 
-    const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID ?? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
 
     return (
         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
